8d343195a464fdab738af6b147a6d63bad9e3cbf
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2026-01-20 14:32:00 +0500

    some changes

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [ReviewerController::class, 'profile']);
     Route::put('profile', [ReviewerController::class, 'updateProfile']);
 
-    Route::prefix('chief-editor')->middleware('role:chief_editor')->group(function () {
+    Route::prefix('chief-editor')->middleware('role:editor')->group(function () {
         Route::get('reviewers/pending', [ChiefEditorController::class, 'pendingReviewers']);
         Route::get('reviewers/approved', [ChiefEditorController::class, 'approvedReviewers']);
         Route::get('reviewers/{id}', [ChiefEditorController::class, 'showReviewer']);
